5ef4ff6570597fefbcf7eb226da62d4ab0402734
Dimas Julio Cessar <dimasjc08@gmail.com>
2022-04-19 22:02:26 +0700

    Add UI Material

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
 
 function Sidebar(){
     return (
@@ -9,6 +13,13 @@ function Sidebar(){
             src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" 
             alt=""
             />
+            <SidebarOption Icon={HomeIcon} title="Home" />
+            <SidebarOption Icon={SearchIcon} title="Search" />
+            <SidebarOption Icon={LibraryMusicIcon} title="Your Liblary" />
+            
+            <br />
+            <strong className="sidebar__title"> PLAYLIST </strong>
+            <hr />
         </div>
     );
 }
